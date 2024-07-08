8a7c6dc37634263676f9c588f636b52a58a8b94d
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2024-07-09 03:47:46 +0600

    Frontend and Backend Connected

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,7 +1,10 @@
 import DashboardBox from "@/components/DashboardBox";
+import {useGetKpisQuery} from "@/state/api.ts";
 
 type Props = {};
 const Row1 = (props: Props) => {
+    const { data } = useGetKpisQuery();
+    console.log('Data:', data)
     return (
         <>
             <DashboardBox gridArea="a">
