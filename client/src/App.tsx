b0de9d2018e3984cc661da7822a564150f3539c2
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2024-07-08 09:35:19 +0600

    Navbar Added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,26 @@
 import {useMemo} from "react";
-import {createTheme} from "@mui/material";
+import {Box, createTheme} from "@mui/material";
 import {themeSettings} from "@/theme.ts";
 import {CssBaseline, ThemeProvider} from "@mui/material";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from "@/scenes/navbar";
 
 function App() {
     const theme = useMemo(() => createTheme(themeSettings), [])
     return (
         <div className="app">
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            Hello
-        </ThemeProvider>
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+                        <Navbar />
+                        <Routes>
+                            <Route path="/" element={ <div>Dashboard</div>} />
+                            <Route path="/predictions" element={<div>Predictions</div>} />
+                        </Routes>
+                    </Box>
+                </ThemeProvider>
+            </BrowserRouter>
     </div>
     );
 }
