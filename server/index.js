052e4e7f85520276c5b85880d300ddd4b0e46111
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2024-07-08 22:46:44 +0600

    mongoose setup

--- server/index.js
+++ server/index.js
@@ -17,4 +17,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-console.log('Hello')
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL, {
+    })
+    .then(async () => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} did not connect`));
